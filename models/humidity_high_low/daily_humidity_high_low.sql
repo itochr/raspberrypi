12140eb2efa7665b5498e2b7356b8e9612d0ba36
Christine Ito <ito.christine88@gmail.com>
2025-04-05 02:45:20 +0000

    Added humidity staging model and updated ref

--- models/humidity_high_low/daily_humidity_high_low.sql
+++ models/humidity_high_low/daily_humidity_high_low.sql
@@ -2,5 +2,5 @@ SELECT
     DATE(timestamp) AS Date,
     MAX(humidity) AS High_Humidity, 
     MIN(humidity) AS Low_Humidity
-FROM {{ ref('Total_Data') }}
+FROM {{ ref('stg_humidity') }}
 GROUP BY Date
