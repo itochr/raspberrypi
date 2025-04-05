24a997a578f7558c9d213ea18bf37bc0a6fab29b
Christine Ito <ito.christine88@gmail.com>
2025-04-05 02:23:08 +0000

    Updated ref to Total_Data

--- models/pressure_high_low/daily_pressure_high_low.sql
+++ models/pressure_high_low/daily_pressure_high_low.sql
@@ -2,5 +2,5 @@ SELECT
     DATE(timestamp) AS Date,
     MAX(pressure) AS High_Pressure,
     MIN(pressure) AS Low_Pressure,
-FROM `pacific-ethos-452022-u8.raspberrypi.raspberry_pi_sense_hat_temperaturedata_tblyfNWl1E5P4J7qx`
+FROM {{ ref('Total_Data') }}
 GROUP BY Date
