56584bffbffab18cb350fd83dbcdea218034b045
Christine Ito <ito.christine88@gmail.com>
2025-04-05 01:51:13 +0000

    Naming consistency in Averages query

--- models/daily_averages/Averages_by_day.sql
+++ models/daily_averages/Averages_by_day.sql
@@ -1,8 +1,8 @@
 SELECT 
   DATE(TIMESTAMP) AS Date, 
-  ROUND(AVG(temp_c),2) AS AVG_TEMP_C, 
-  ROUND(AVG(temp_c),2) AS AVG_TEMP_F, 
-  ROUND(AVG(pressure),2) AS AVG_PRESSURE, 
-  ROUND(AVG(humidity),2) AS AVG_HUMIDITY
+  ROUND(AVG(temp_c),2) AS Avg_Temp_C, 
+  ROUND(AVG(temp_c),2) AS Avg_Temp_F, 
+  ROUND(AVG(pressure),2) AS Avg_Pressure, 
+  ROUND(AVG(humidity),2) AS Avg_Humidity
 FROM {{ ref('Total_Data') }} 
 GROUP BY Date
